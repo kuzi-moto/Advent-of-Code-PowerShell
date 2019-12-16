4b43395527258d7a775a8fc6f2ce467d13f5f5e8
kuzi-moto <kuzimoto@gmail.com>
2019-12-16 10:56:18 -0600

    works

--- 2019/Day 6/P1.ps1
+++ 2019/Day 6/P1.ps1
@@ -4,6 +4,44 @@ Param (
 
 $Data = Get-Content $InputFile
 
-# Solution goes here
+$Map = @{ }
 
-Write-Host "Answer: "
+$Data | ForEach-Object {
+  $_ -match '(\w+)\)(\w+)' | Out-Null
+  $one = $Matches[1]
+  $two = $Matches[2]
+  if (!$Map.$One) { $Map.$one = $null }
+  $Map.$two = $one
+}
+
+$Queue = @()
+
+$Map.Clone().GetEnumerator() | Where-Object { !$_.Value } | ForEach-Object {
+  $Queue += $_.Name
+  $Map.Remove($_.Name)
+}
+
+$Counts = @{}
+$Count = 1
+
+do {
+  $Map.Count
+  $NewQueue = @()
+
+  $Map.Clone().GetEnumerator() | Where-Object { $Queue -contains $_.Value } | ForEach-Object {
+    $Satellite = $_.Name
+    $Counts.$Satellite = $Count
+    $NewQueue += $Satellite
+    $Map.Remove($_.Name)
+  }
+
+  $Queue = $NewQueue.Clone()
+  $Count++
+} until ($Queue.Count -eq 0)
+
+$Sum = 0
+$Counts.Values | ForEach-Object {$Sum += $_ }
+
+$Ans = $Sum
+
+Write-Host "Total # of orbits: $Ans"
