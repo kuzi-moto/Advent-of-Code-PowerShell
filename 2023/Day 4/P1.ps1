3728b3f3c59af0762b57e586560998fe3ca402dd
kuzi-moto <kuzimoto@gmail.com>
2023-12-06 08:29:00 -0600

    Correct D4P1 2023

--- 2023/Day 4/P1.ps1
+++ 2023/Day 4/P1.ps1
@@ -1,40 +1,34 @@
 Param (
-  $InputFile = (Join-Path $PSScriptRoot 'Input.txt')
+  $InputFile = (Join-Path $PSScriptRoot "Input.txt")
 )
 
 $Data = Get-Content $InputFile
-$CardTable = @{}
+$Sum = 0
 
 for ($i = 0; $i -lt $Data.Count; $i++) {
-  $CardTable.$i = 1
-}
+  $Game = ($Data[$i] -split ':')[1]
+  $Numbers = $Game -split '\|'
+  $Points = 0
 
-for ($i = 0; $i -lt $Data.Count; $i++) {
+  [array]$WinningNums = ($Numbers[0] | Select-String '\d+' -AllMatches).Matches.Value | ForEach-Object { [int]$_ }
+  [array]$MyNums = ($Numbers[1] | Select-String '\d+' -AllMatches).Matches.Value | ForEach-Object { [int]$_ }
 
-  for ($ii = 0; $ii -lt $CardTable.$i.Count; $ii++) {
-    $Game = ($Data[$i] -split ':')[1]
-    $Numbers = $Game -split '\|'
-    $Points = 0
+  $MatchingNums = Compare-Object -ReferenceObject $WinningNums -DifferenceObject $MyNums -ExcludeDifferent | Select-Object -ExpandProperty InputObject
 
-    [array]$WinningNums = ($Numbers[0] | Select-String '\d+' -AllMatches).Matches.Value | ForEach-Object { [int]$_ }
-    [array]$MyNums = ($Numbers[1] | Select-String '\d+' -AllMatches).Matches.Value | ForEach-Object { [int]$_ }
-
-    $MatchingNums = Compare-Object -ReferenceObject $WinningNums -DifferenceObject $MyNums -ExcludeDifferent | Select-Object -ExpandProperty InputObject
-
-    for ($ii = 0; $ii -lt $MatchingNums.Count; $ii++) {
-      if ($ii -eq 0) {
-        $Points = 1
-      }
-      else {
-        $Points *= 2
-      }
+  for ($ii = 0; $ii -lt $MatchingNums.Count; $ii++) {
+    if ($ii -eq 0) {
+      $Points = 1
     }
-
+    else {
+      $Points *= 2
+    }
   }
+
+  $Sum += $Points
 
 }
 
 # Solution goes here
-# Answer 1: 
+# Answer 1: 27059
 
 Write-Host "Answer: $Sum"
